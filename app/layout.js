f0a67a6f34b44142f166b435f37984be90935668
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-11-28 08:54:56 -0300

    feat: load fonts

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,15 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+console.log(josefin)
+
 import "@/app/_styles/globals.css";
 
 export const metadata = {
@@ -8,13 +17,14 @@ export const metadata = {
     template: "%s The Wild Oasis",
     default: "Welcome / The Wild Oasis",
   },
-  description: "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests"
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-neutral-950 text-neutral-100 min-h-screen">
+      <body className={`${josefin.className} bg-neutral-950 text-neutral-100 min-h-screen`}>
         <header>
           <Logo />
           <Navigation />
