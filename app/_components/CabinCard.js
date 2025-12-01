7b724ba6884eb18662338990d943089efcc8fb3e
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-12-01 18:53:59 -0300

    feat: fetch and display cabin list

--- app/_components/CabinCard.js
+++ app/_components/CabinCard.js
@@ -1,25 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
 import { UsersIcon } from "@heroicons/react/24/solid";
 
 function CabinCard({ cabin }) {
   const { id, name, maxCapacity, regularPrice, discount, image } = cabin;
 
   return (
-    <div className="flex border-primary-800 border">
-      <img
-        src={image}
-        alt={`Cabin ${name}`}
-        className="flex-1 border-r border-primary-800"
-      />
-
+    <div className="flex border-neutral-800 border">
+      <div className="flex-1 relative">
+        <Image
+          src={image}
+          fill
+          alt={`Cabin ${name}`}
+          className="object-cover border-r border-neutral-800"
+        />
+      </div>
       <div className="flex-grow">
-        <div className="pt-5 pb-4 px-7 bg-primary-950">
-          <h3 className="text-accent-500 font-semibold text-2xl mb-3">
+        <div className="pt-5 pb-4 px-7 bg-neutral-950">
+          <h3 className="text-orange-300 font-semibold text-2xl mb-3">
             Cabin {name}
           </h3>
 
           <div className="flex gap-3 items-center mb-2">
-            <UsersIcon className="h-5 w-5 text-primary-600" />
-            <p className="text-lg text-primary-200">
+            <UsersIcon className="h-5 w-5 text-neutral-600" />
+            <p className="text-lg text-neutral-200">
               For up to <span className="font-bold">{maxCapacity}</span> guests
             </p>
           </div>
@@ -30,24 +34,24 @@ function CabinCard({ cabin }) {
                 <span className="text-3xl font-[350]">
                   ${regularPrice - discount}
                 </span>
-                <span className="line-through font-semibold text-primary-600">
+                <span className="line-through font-semibold text-neutral-600">
                   ${regularPrice}
                 </span>
               </>
             ) : (
               <span className="text-3xl font-[350]">${regularPrice}</span>
             )}
-            <span className="text-primary-200">/ night</span>
+            <span className="text-neutral-200">/ night</span>
           </p>
         </div>
 
-        <div className="bg-primary-950 border-t border-t-primary-800 text-right">
-          <a
+        <div className="bg-neutral-950 border-t border-t-neutral-800 text-right">
+          <Link
             href={`/cabins/${id}`}
-            className="border-l border-primary-800 py-4 px-6 inline-block hover:bg-accent-600 transition-all hover:text-primary-900"
+            className="border-l border-neutral-800 py-4 px-6 inline-block hover:bg-orange-400 transition-all hover:text-neutral-900"
           >
             Details & reservation &rarr;
-          </a>
+          </Link>
         </div>
       </div>
     </div>
